b0183a9cb2d5c13d93eb4d02bedf29b0a1767811
derkrisz <krisztian.dergmail.com>
2018-01-23 20:31:58 +0000

    dinosaur has type and offspring

---
+++ dinosaur.js
@@ -0,0 +1,6 @@
+const Dinosaur = function(type, offspring) {
+  this.type = type;
+  this.offpsring = offspring;
+}
+
+module.exports = Dinosaur;
---
+++ specs/dinosaur_specs.js
@@ -0,0 +1,19 @@
+const assert = require('assert');
+const Dinosaur = require('../dinosaur.js');
+
+describe('Dinosaur', function (){
+
+  var dinosaur;
+
+  beforeEach (function(){
+    dinosaur = new Dinosaur("Velociraptor", 2);
+  });
+
+  it('dinosaur has type', function(){
+    assert.strictEqual(dinosaur.type, "Velociraptor");
+  });
+
+  it('dinosaur has offspring(s)', function(){
+    assert.strictEqual(dinosaur.offpsring, 2);
+  });
+});
